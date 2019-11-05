05313025a29743ae88d341d1eabf60974b53b56d
ArtyMandy <artur.mandybura@googlemail.com>
2019-11-05 02:08:16 +0100

    dockerfile works now with shell script

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "hello world"
+
+ES_DB="localhost"
+ES_PORT="9200"
+DB_NAME="kibble"
+MAILHOST="localhost:25"
+SHARDS="5"
+REPLICA="1"
+cd /var/www/kibble/setup/
+python3 setup.py --skiponexist --autoadmin -e $ES_DB -p $ES_PORT -d $DB_NAME -m $MAILHOST -s $SHARDS -r $REPLICA
+
+cd /var/www/kibble/api/
+gunicorn3 -w 10 -b 127.0.0.1:8000 handler:application -t 120 -D
+
+apachectl -D FOREGROUND
