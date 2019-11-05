bd8cf834bc0bf6d8eb44a43ae3d145dd27f2ab25
ArtyMandy <artur.mandybura@googlemail.com>
2019-11-05 16:41:25 +0100

    docker-compose should work now

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 echo "hello world"
 
-ES_DB="localhost"
+echo $1
+
+ES_DB="elasticsearch"
 ES_PORT="9200"
 DB_NAME="kibble"
-MAILHOST="localhost:25"
+MAILHOST="127.0.0.1:25"
 SHARDS="5"
 REPLICA="1"
+
+echo "wait for ES"
+
+result=$(curl --head --write-out %{http_code} --silent --output /dev/null $1)
+
+echo $result
+
+while [ $result != "200" ]
+do
+  sleep 5
+  echo $result
+  result=$(curl --head --write-out %{http_code} --silent --output /dev/null $1)
+done
+break
+
+
 cd /var/www/kibble/setup/
 python3 setup.py --skiponexist --autoadmin -e $ES_DB -p $ES_PORT -d $DB_NAME -m $MAILHOST -s $SHARDS -r $REPLICA
 
